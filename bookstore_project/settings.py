cc3a502628a65c850ffcd3add3c4970ce62afbf8
TareqMonwer <tareqmonwer137@gmail.com>
2019-12-26 02:55:32 +0600

    chapter-6 static files

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -26,10 +26,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # third party apps
+    'crispy_forms',
+
     # local apps
     'users.apps.UsersConfig',
     'pages.apps.PagesConfig',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -114,6 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_FINDER = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
 
 
 # Custom user model
--- pages/tests.py
+++ pages/tests.py
@@ -1,7 +1,7 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 
-from .views import HomePageView
+from .views import HomePageView, AboutPageView
 
 
 class HomePageTests(SimpleTestCase):
@@ -27,3 +27,27 @@ class HomePageTests(SimpleTestCase):
         view = resolve("/")
         self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
 
+
+class AboutPageTest(SimpleTestCase):
+    def setUp(self):
+        url = reverse('about')
+        self.resp = self.client.get(url)
+    
+    def test_about_page_status_code(self):
+        self.assertEqual(self.resp.status_code, 200)
+
+    def test_about_page_template(self):
+        self.assertTemplateUsed(self.resp, 'about.html')
+
+    def test_about_page_contains_html(self):
+        self.assertContains(self.resp, 'About Page')
+
+    def test_about_page_does_not_contain(self):
+        self.assertNotContains(self.resp, 'Invalid Text')
+
+    def test_about_page_resolve_view(self):
+        view = resolve('/about/')
+        self.assertEqual(
+            view.func.__name__,
+            AboutPageView.as_view().__name__
+        )
