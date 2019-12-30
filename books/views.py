bd1e97609a2fa72a1a5b01b2e7156f13260ac463
TareqMonwer <tareqmonwer137@gmail.com>
2019-12-30 07:18:47 +0600

    search via Q

--- books/views.py
+++ books/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
@@ -26,3 +27,15 @@ class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     def handle_no_permission(self):
         ''' AccessMixin's method overriden '''
         return redirect('authorization-required')
+
+
+class SearchResultsListView(ListView):
+    model = Book
+    context_object_name = 'books'
+    template_name = 'books/search_results.html'
+    
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return Book.objects.filter(
+            Q(title__icontains=query) | Q(author__icontains=query)
+        )
